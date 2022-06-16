1b90bfcd9983ac1eec32d421a14fd3e136107ec1
Hayden Sweet <haydensweet22@gmail.com>
2022-06-16 12:11:43 -0600

    git ignore added

--- markov.py
+++ markov.py
@@ -1,4 +1,4 @@
-"""Generate Markov text from text files."""
+
 
 from cgitb import text
 from hashlib import new
@@ -7,11 +7,6 @@ import random
 
 
 def open_and_read_file(file_path):
-    """Take file path as string; return text as string.
-
-    Takes a string that is a file path, opens the file, and turns
-    the file's contents as one string of text.
-    """
 
     contents = open(file_path).read()
 
@@ -19,30 +14,6 @@ def open_and_read_file(file_path):
 
 
 def make_chains(text_string):
-    """Take input text as string; return dictionary of Markov chains.
-
-    A chain will be a key that consists of a tuple of (word1, word2)
-    and the value would be a list of the word(s) that follow those two
-    words in the input text.
-
-    For example:
-
-        >>> chains = make_chains('hi there mary hi there juanita')
-
-    Each bigram (except the last) will be a key in chains:
-
-        >>> sorted(chains.keys())
-        [('hi', 'there'), ('mary', 'hi'), ('there', 'mary')]
-
-    Each item in chains is a list of all possible following words:
-
-        >>> chains[('hi', 'there')]
-        ['mary', 'juanita']
-
-        >>> chains[('there','juanita')]
-        [None]
-    """
-
     chains = {}
 
     words = text_string.split()
@@ -63,7 +34,6 @@ def make_chains(text_string):
 
 
 def make_text(chains):
-    """Return text from chains."""
     words = []
     original_pair = list(chains.keys())[0]
     words.append(original_pair[0])
@@ -82,7 +52,7 @@ def make_text(chains):
     return ' '.join(words)
     
 
-input_path = 'gettysburg.txt'
+input_path = 'green-eggs.txt'
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
